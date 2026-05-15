a2ae9dee243b2d668c9e5772e35e1760e6b314e8
Nitin Premanand <nitinpremanand@Nitins-MacBook-Pro.local>
2026-05-15 22:53:52 +0530

    Ep4: Url patterns

--- src/app.js
+++ src/app.js
@@ -15,6 +15,42 @@ app.use("/test", (req, res) => {
 //     res.send("Hello from the server! User...");
 // });
 
+// b is optional, Works for /abc, /ac
+app.get("/ab?c", (req, res) => {
+    res.send({ firstName: "Nitin", lastName: "Premanand"})
+});
+
+// bc is optional, Works for /abcd, /ad
+// Not works for /acd
+app.get("/a(bc)?d", (req, res) => {
+    res.send({ firstName: "Nitin", lastName: "Premanand"})
+});
+
+// b can be more than once, Works for /abc, /abbbbbbbbbbbc
+// Not works for /abcc
+app.get("/ab+c", (req, res) => {
+    res.send({ firstName: "Nitin", lastName: "Premanand"})
+});
+
+// bc can be more than once, Works for /abcd, /abcbcbcbd
+// Not works for /acd
+app.get("/a(bc)+d", (req, res) => {
+    res.send({ firstName: "Nitin", lastName: "Premanand"})
+});
+
+// It should start with ab & end with cd anything inbetween accepted, Works for /abHAHAHAHcd
+app.get("/ab*cd", (req, res) => {
+    res.send({ firstName: "Nitin", lastName: "Premanand"})
+});
+
+app.get(/a/, (req, res) => {
+    res.send({ firstName: "Nitin", lastName: "Premanand"})
+});
+app.get(/.^fly$/, (req, res) => {
+    res.send({ firstName: "Nitin", lastName: "Premanand"})
+});
+
+// Works for /user, /user/xyz, /user/1
 app.get("/user", (req, res) => {
     res.send({ firstName: "Nitin", lastName: "Premanand"})
 });
